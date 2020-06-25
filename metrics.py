022c48101df317ea7c46d6a15a892c2000d5ed07
Juraj Seffer <github@juraj.io>
2020-06-25 02:58:02 +0100

    INFRA-866 AWS Lambda Python 3.8 compatibility; datadog token

--- metrics.py
+++ metrics.py
@@ -19,36 +19,59 @@ import datadog
 from common import AV_STATUS_CLEAN
 from common import AV_STATUS_INFECTED
 
+import boto3
+from botocore.exceptions import ClientError
+
+def get_datadog_api_key():
+    secret_name = os.environ.get("DATADOG_API_KEY_SECRET_NAME")
+    region_name = os.environ.get("AWS_REGION", "")
+
+    if not region_name:
+        raise Exception('Empty AWS Region, AWS_REGION variable must be set')
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    return get_secret_value_response['SecretString']
 
 def send(env, bucket, key, status):
-    if "DATADOG_API_KEY" in os.environ:
-        datadog.initialize()  # by default uses DATADOG_API_KEY
+    if os.environ.get("AV_DATADOG_INTEGRATION_ENABLED", "false") == "true":
+        if "DATADOG_API_KEY" in os.environ:
+            api_key = os.environ.get("DATADOG_API_KEY")
+        else:
+            api_key = get_datadog_api_key()
 
-        result_metric_name = "unknown"
+        datadog.initialize(api_key=api_key)
 
-        metric_tags = ["env:%s" % env, "bucket:%s" % bucket, "object:%s" % key]
+            result_metric_name = "unknown"
 
-        if status == AV_STATUS_CLEAN:
-            result_metric_name = "clean"
-        elif status == AV_STATUS_INFECTED:
-            result_metric_name = "infected"
-            datadog.api.Event.create(
-                title="Infected S3 Object Found",
-                text="Virus found in s3://%s/%s." % (bucket, key),
-                tags=metric_tags,
-            )
+            metric_tags = ["env:%s" % env, "bucket:%s" % bucket, "object:%s" % key]
 
-        scanned_metric = {
-            "metric": "s3_antivirus.scanned",
-            "type": "counter",
-            "points": 1,
-            "tags": metric_tags,
-        }
-        result_metric = {
-            "metric": "s3_antivirus.%s" % result_metric_name,
-            "type": "counter",
-            "points": 1,
-            "tags": metric_tags,
-        }
-        print("Sending metrics to Datadog.")
-        datadog.api.Metric.send([scanned_metric, result_metric])
+            if status == AV_STATUS_CLEAN:
+                result_metric_name = "clean"
+            elif status == AV_STATUS_INFECTED:
+                result_metric_name = "infected"
+                datadog.api.Event.create(
+                    title="Infected S3 Object Found",
+                    text="Virus found in s3://%s/%s." % (bucket, key),
+                    tags=metric_tags,
+                )
+
+            scanned_metric = {
+                "metric": "s3_antivirus.scanned",
+                "type": "counter",
+                "points": 1,
+                "tags": metric_tags,
+            }
+            result_metric = {
+                "metric": "s3_antivirus.%s" % result_metric_name,
+                "type": "counter",
+                "points": 1,
+                "tags": metric_tags,
+            }
+            print("Sending metrics to Datadog.")
+            datadog.api.Metric.send([scanned_metric, result_metric])
